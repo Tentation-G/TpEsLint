d8ff30e0cd01fe52d0d2e9a459413bc5ae8e9cec
Tentation-G <f.ca3@aol.com>
2026-02-10 16:49:52 +0100

    Fix eslint config formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-   files: ["**/*.{js,mjs,cjs}"],
-   plugins: { js },
-   languageOptions: {
-   globals: globals.node,
-   ecmaVersion: "latest",
-   sourceType: "commonjs"
-   },
-   rules: {
-   "indent": ["error", 2],
-   "quotes": ["error", "single"],
-   "semi": ["error", "always"],
-   "no-console": "warn",
-   "no-unused-vars": "error"
-   }
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-vars': 'error',
+
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'space-before-function-paren': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never']
+    }
   },
   js.configs.recommended
-  ]);
+]);
